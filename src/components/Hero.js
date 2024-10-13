88929e9d4d0dbf4f18025ea84d635ff76a3e584e
akshatparashar <akshat.parashar@fortytwo42.in>
2024-10-14 00:56:57 +0530

    Hero Section And Navbar changes For Sample

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavBar from '../components/Navbar/NavBar';
-import heroImg from '../images/web-dev.svg';
+import heroImg from '../images/hero-laundary.svg';
 
 const Hero = () => {
     return (
@@ -17,9 +17,9 @@ const Hero = () => {
                         <div className="lg:w-1/2 flex flex-col justify-center" data-aos="zoom-in" data-aos-delay="200">
                             <h1 className="mb-5 md:text-5xl text-3xl font-bold text-blue-900">
                             {/* We build digital solutions to help businesses scale */}
-                                Bespoke software solutions for your unique business needs
+                                Your Fresh Start: Premium Laundry Services at Your Doorstep!
                             </h1>
-                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled developers dedicated to delivering only the best software.</div>
+                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">Convenient, eco-friendly laundry services tailored to your needs.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
                                 <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
                                     Learn more
